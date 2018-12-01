cef2ccc02dbac15cac9895cf44bb3bf659d2a1ea
zenolab <32515564+zenolab@users.noreply.github.com>
2018-12-01 22:04:01 +0200

    cleaning

--- app/src/main/java/com/zenolab/nav/grd/mp3simple/pickertimer/rxbus/RxBusActivity.java
+++ app/src/main/java/com/zenolab/nav/grd/mp3simple/pickertimer/rxbus/RxBusActivity.java
@@ -6,11 +6,6 @@ import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
 
-
-//import com.rxjava2.android.samples.MyApplication;
-//import com.rxjava2.android.samples.R;
-//import com.rxjava2.android.samples.model.RxEvents;
-
 import com.zenolab.nav.grd.mp3simple.pickertimer.MyApplication;
 import com.zenolab.nav.grd.mp3simple.pickertimer.R;
 
@@ -23,25 +18,18 @@ import io.reactivex.schedulers.Schedulers;
  * Created by grd on 4/9/18.
  */
 
-//public class RxBusActivity{}
-// /*
 public class RxBusActivity extends AppCompatActivity {
 
     public static final String TAG = RxBusActivity.class.getSimpleName();
     TextView textView;
     Button button;
-    //for many syscribers ( implements observer)
-    /*
-    По аналогии с составной подпиской из RxJava есть и составной Disposable:
-    вы можете подписаться на несколько источников, взять возвращаемые Disposable,
-    добавить их в CompositeDisposable и одновременно отписаться от всех потоков.
-     */
+    
     private final CompositeDisposable disposables = new CompositeDisposable();
 
     @Override
     protected void onDestroy() {
         super.onDestroy();
-        disposables.clear(); // do not send event after activity has been destroyed
+        disposables.clear(); 
     }
 
     @Override
@@ -52,24 +40,6 @@ public class RxBusActivity extends AppCompatActivity {
         textView = (TextView) findViewById(R.id.textView);
         button = (Button) findViewById(R.id.button);
 
-
-       // disposables about - https://habr.com/company/badoo/blog/328434/
-        //одноразовые
-        /* Колбэк onSubscribe вызывается сразу же, как только вы начинаете прослушивать Observable или Flowable,
-         и он передаст вам объект одного из двух типов: Disposable или Subscription
-
-
-        Применительно к Observable тип Disposable позволяет вызывать метод dispose,
-        означающий «Я закончил работать с этим ресурсом, мне больше не нужны данные».
-        Если у вас есть сетевой запрос, то он может быть отменён.
-        Если вы прослушивали бесконечный поток нажатий кнопок, то это будет означать, что вы больше не хотите получать эти события,
-         в таком случае можно удалить OnClickListener у View.
-
-
-        Всё это справедливо и для интерфейса Subscription. Хоть он и называется иначе,
-         но используется точно так же: у него есть метод cancel(), аналогичный dispose().
-
-         */
         disposables.add(((MyApplication) getApplication())
                 .bus()
                 //own method
@@ -90,7 +60,7 @@ public class RxBusActivity extends AppCompatActivity {
                     }
                 }));
 
-        //link on onClick() xml - MainActiivty
+        //onClick() xml (MainActiivty)
         button.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
@@ -101,8 +71,5 @@ public class RxBusActivity extends AppCompatActivity {
         });
     }
 
-
-
 }
 
-// */
